703d8a31f224389b4e8e963d2de9531a054aa6ed
HarshPatil32 <patilsharsh@gmail.com>
2025-02-14 03:20:38 -0800

    Add eye detection, minNeighbors really high because it kept saying my nostrils are an eye

--- identify_face.py
+++ identify_face.py
@@ -1,22 +1,27 @@
 import cv2
-import threading
 
 # Load OpenCV's pre-trained Haar cascade face detector
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 def detect_face():
-    cap = cv2.VideoCapture(1)  # Access webcam
+    cap = cv2.VideoCapture(0)  # Access webcam
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
         
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)  # Detect faces
+        faces = face_cascade.detectMultiScale(gray, 1.05, 20)  # Detect faces
         
         # Draw rectangles around detected faces
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+            roi_gray = gray[y:y+w, x:x+w]
+            roi_color = frame[y:y+h, x:x+h]
+            eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 20)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
 
         cv2.imshow("Face Detection", frame)
 
